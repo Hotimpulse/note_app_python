ff45443fddb77000ed76a3777ff53fbc2b2d718a
Lev <hotimpulse@hotmail.com>
2023-02-12 22:24:51 +0700

    feat: date filter to option 3 and 4

--- note.py
+++ note.py
@@ -1,5 +1,6 @@
 class Note:
-    def __init__(self, note_id, note_head, note_text):
+    def __init__(self, note_id, note_head, note_text, note_edit):
      self.note_id = note_id;
      self.note_head = note_head;
      self.note_text = note_text;
+     self.note_edit = note_edit;
